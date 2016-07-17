40787a4a72b4c53ab7cf65a3050153183799b92f
Anvita Mishra <anvitamishra309@gmail.com>
2016-07-17 18:02:28 -0400

    Removed HAProxy stuff

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,5 @@
 'use strict';
 
-var HAProxy = require('haproxy');
-var haproxy = new HAProxy('/optional/socket/path.sock', { /*options */ });
-
 var appRouter = function(app) {
 	app.get("/", function(req, res) {
 		res.send("Hello World");
