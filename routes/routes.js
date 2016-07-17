49dc68e80f5561942abe08164b8ac4321f0e0890
Anvita Mishra <anvitamishra309@gmail.com>
2016-07-17 17:59:02 -0400

    Removed some random code

--- routes/routes.js
+++ routes/routes.js
@@ -8,28 +8,6 @@ var appRouter = function(app) {
 		res.send("Hello World");
 	});
 	
-	// app.get("/account", function(req, res) {
-	// 	var accountMock = {
-	// 		"username": "amishra",
-	// 		"password": "1234",
-	// 		"instagram": "@anvitamishra"
-	// 	}
-	// 	if(!req.query.username) {
-	// 		return res.send({"status": "error", "message": "missing"});
-	// 	} else if (req.query.username != accountMock.username) {
-	// 		return res.send({"status": "error", "message": "wrong username"});
-	// 	} else {
-	// 		return res.send(accountMock);
-	// 	}
-	// });
-	
-	// app.post("/account", function(req, res) {
-	// 	if(!req.body.username || !req.body.password || !req.body.instagram) {
-	// 		return res.send({"status": "error", "message": "missing a parameter"});
-	// 	} else {
-	// 		return res.send(req.body);
-	// 	}
-	// });
 }
 	
 module.exports = appRouter;
