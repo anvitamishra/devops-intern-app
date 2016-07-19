b2dd73ea9d8352ab41851da470923d05c9b4bfe9
Anvita Mishra <anvitamishra309@gmail.com>
2016-07-18 21:25:08 -0400

    Minor changes

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 var baseURL = "http://localhost:3000/";
 var server = require("../app.js");
 
-describe("Hello World App Server", function() {
+describe("Hello Unu App Server", function() {
 	describe("GET /", function() {
 		it("return status 200 OK", function(done) {
 			request.get(baseURL, function(error, response, body) {
@@ -11,9 +11,9 @@ describe("Hello World App Server", function() {
 				done();
 			});
 		});
-		it("get Hello World", function(done) {
+		it("get Hello Unu", function(done) {
 			request.get(baseURL, function(error, response, body) {
-				assert.equal("Hello World", body);
+				assert.equal("Hello Unu", body);
 				server.closeServer();
 				done();
 			});
